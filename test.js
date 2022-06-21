44974c56e7b72e91056fb4ac7446fefb6bda03fa
Clemens Schneider <clemens.schneider.ch@gmail.com>
2022-06-21 21:52:18 +0200

    Update

---
+++ test.js
@@ -0,0 +1,22 @@
+import { 
+    getCollection, 
+    getDetails 
+} from '@geolocation-services/nextparkinglot-package'
+
+
+var parkingLots = 
+    await getCollection(47.81044, 50.0704, 8.43979, 12.08176, 9);
+
+parkingLots.forEach(parkingLot => console.log(`
+    Id: ${parkingLot.Id}
+    Latitude: ${parkingLot.Latitude}
+    Longitude: ${parkingLot.Longitude}
+`));
+
+
+var details = await getDetails(28588151);
+
+details.forEach(detail => console.log(`
+    Key: ${detail.Key}
+    Value: ${detail.Value}
+`));
